1b448c716e13454661f2a189dc3408fbbf862afe
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2016-03-10 15:00:00 +0100

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>Clicked {{numClicks}} times</h1>
+    <h2>{{greeting}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    greeting: String,
+    numClicks: Number
+  },
+
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
